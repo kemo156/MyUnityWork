1048da55e2bf8cc822f808811c589e50a832da8b
Kim <kimbelongilot@gmail.com>
2019-05-03 23:12:12 +0800

    added vid 12

---
+++ inscope version2/Inscope/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+
+    private BoxCollider2D playerCollider;
+
+    [SerializeField]
+    private BoxCollider2D platformCollider;
+
+    [SerializeField]
+    private BoxCollider2D platformTrigger; //Do this after step 11
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(platformCollider, platformTrigger, true); //do this once you are done on step 11
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)//11:29
+    {
+        
+    }
+}
